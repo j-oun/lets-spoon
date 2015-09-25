4dc67c22967d6a722f63d07cf8dfc26a9e6a681e
freemer <pushpaw@gmail.com>
2015-09-25 23:08:28 +0000

    Search working, filters not

--- app/actions.rb
+++ app/actions.rb
@@ -20,35 +20,36 @@ get '/search' do
   @search_term = params[:search_term]
   diet = @user.diet_id
   @recipes = Recipe.find_by_sql(
-    "SELECT recipes.name
-    FROM recipes 
-    JOIN recipe_ingredients 
-      ON recipes.id = recipe_ingredients.recipe_id
-    JOIN ingredients
-      ON recipe_ingredients.ingredient_id = ingredients.id
-    WHERE recipes.id NOT IN (
-      SELECT recipes.id 
+    "SELECT *
       FROM recipes 
       JOIN recipe_ingredients 
         ON recipes.id = recipe_ingredients.recipe_id
-      WHERE recipe_ingredients.ingredient_id IN ( 
-        SELECT ingredient_id 
-        FROM banned_ingredients 
-        JOIN diets 
-          ON banned_ingredients.diet_id = diets.id
-       WHERE diets.id = #{diet}
-      )
+      JOIN ingredients
+        ON recipe_ingredients.ingredient_id = ingredients.id
+      WHERE (
+        recipes.name LIKE '%#{@search_term}%'
+        OR recipes.description LIKE '%#{@search_term}%'  
+        OR ingredients.name LIKE '%#{@search_term}%' 
+      )  
+      AND recipes.id NOT IN (
+        SELECT recipes.id 
+        FROM recipes 
+        JOIN recipe_ingredients 
+          ON recipes.id = recipe_ingredients.recipe_id
+        WHERE recipe_ingredients.ingredient_id IN ( 
+          SELECT ingredient_id 
+          FROM banned_ingredients 
+          JOIN diets ON banned_ingredients.diet_id = diets.id
+          WHERE diets.id = 4
     )
-    AND (
-      recipes.name LIKE '#{@search_term}'
-      OR recipes.description LIKE '(#{@search_term})'
-      OR ingredients.name LIKE '(#{@search_term})'
-    )  
-    GROUP BY recipes.id")
+  )
+  
+  GROUP BY recipes.id;")
+
+#{@search_term}
+  # byebug
 
   erb :'search/results'
-
-  # erb :index
 end
 
 get '/recipes/:id' do
